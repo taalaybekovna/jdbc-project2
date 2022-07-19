30f293ac5d650289c389a35d3f253e52dbeec0d5
Mehmet <mhmtsytrk@gmail.com>
2022-07-19 19:32:33 -0400

    day02

---
+++ src/test/resources/Queries/day02/review.sql
@@ -0,0 +1,32 @@
+select * from employees;
+
+-- Review
+
+--  Display all infromation  where job title President ,Sales Manager, Finance manager
+select * from JOBS
+where JOB_TITLE='President' or JOB_TITLE='Sales Manager' or JOB_TITLE='Finance Manager';
+
+select * from JOBS
+where JOB_TITLE IN ('President','Sales Manager','Finance Manager');
+
+-- Display all Department that does not have manager_id in departments table
+select * from DEPARTMENTS
+where MANAGER_ID IS NULL ;
+
+--  Display all Locations in US or UK from Locations table
+select * from LOCATIONS
+where COUNTRY_ID IN('UK','US');
+
+--  Display all Locations except US or UK from Locations table
+select * from LOCATIONS
+where COUNTRY_ID NOT IN('UK','US');
+
+--  Display all Countries where Region_ID is 1 and Country_Name not Belgium
+select * from COUNTRIES
+where REGION_ID=1 and COUNTRY_NAME!='Belgium' ;
+
+select * from COUNTRIES
+where REGION_ID=1 and COUNTRY_NAME<>'Belgium' ;
+
+select * from COUNTRIES
+where REGION_ID=1 and COUNTRY_NAME not in  ('Belgium') ;
