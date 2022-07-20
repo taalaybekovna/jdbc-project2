366af964cc793e7d9522e6176a40a13d85fdf2f4
Mehmet <mhmtsytrk@gmail.com>
2022-07-19 21:33:24 -0400

    day02

--- src/test/resources/Queries/day02/MultiRowFunctions.sql
+++ src/test/resources/Queries/day02/MultiRowFunctions.sql
@@ -9,6 +9,9 @@ select * from locations ;
  sum
 
  it will take multiple row and gives single output
+
+ All aggregate functions ignore null values
+
  */
 
 
@@ -28,6 +31,48 @@ select count(*)  from EMPLOYEES
 where JOB_ID IN('IT_PROG','SA_REP');
 
 -- NOTE --> If you wanna count your result use * to get correct answer
+
+-- How many employees we have as department_id is null
+-- This gives us correct result since it counting the rows
+select count(*) from EMPLOYEES
+where DEPARTMENT_ID is null ;
+
+-- This gives us wrong data if you wanna count rows, since aggregate functions ignore the null values
+select count(DEPARTMENT_ID) from EMPLOYEES
+where DEPARTMENT_ID is null ;
+
+-- This bot h gives you same result since there is no null the result
+select count(*) from EMPLOYEES
+where DEPARTMENT_ID is not null ;
+
+select count(DEPARTMENT_ID) from EMPLOYEES
+where DEPARTMENT_ID is not null ;
+
+
+--MAX
+-- get me max salary from meployees
+select max(salary) from EMPLOYEES;
+
+--MIN
+-- get me min salary from meployees
+select min(salary) from EMPLOYEES;
+
+
+-- SUM
+-- get me total salary of employees
+select sum(salary) from EMPLOYEES;
+
+
+--AVG
+-- get me avg salary rfrom employees
+select avg(salary) from EMPLOYEES;
+
+
+-- ROUND --> It is not an aggregate func.It is numeric function
+select round(avg(salary)) from EMPLOYEES;
+select round(avg(salary),1) from EMPLOYEES;
+select round(avg(salary),2) from EMPLOYEES;
+select round(avg(salary),3) from EMPLOYEES;
 
 
 
