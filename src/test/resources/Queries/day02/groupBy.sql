e34d095c581a13bfe7838171c54352e84c7890b7
Mehmet <mhmtsytrk@gmail.com>
2022-07-20 20:14:54 -0400

    day03

--- src/test/resources/Queries/day02/groupBy.sql
+++ src/test/resources/Queries/day02/groupBy.sql
@@ -32,5 +32,29 @@ group by DEPARTMENT_ID
 order by DEPARTMENT_ID;
 
 
+-- get me how many departments we have in each location
+select  * from DEPARTMENTS;
+
+select LOCATION_ID,count(*) from DEPARTMENTS
+group by LOCATION_ID;
 
 
+-- get me how many locations we have in each country
+select COUNTRY_ID,count(*) from LOCATIONS
+group by COUNTRY_ID
+order by count(*) asc;  -- to sort them based on nuber of locations
+                        -- asc is default but still we can use
+
+-- get me how many countries we have in each region
+select REGION_ID,count(*) from COUNTRIES
+group by REGION_ID
+order by REGION_ID;      -- order them based on region_id
+
+
+-- get me how many employees working for each manager
+select MANAGER_ID,count(*) from EMPLOYEES
+                           where MANAGER_ID IS NOT NULL --ignore null values
+group by MANAGER_ID
+having count(*)>5;
+
+--
