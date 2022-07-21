8faff4f6913c70fadccba6bccb489c4d7f3b19a5
Mehmet <mhmtsytrk@gmail.com>
2022-07-20 20:40:19 -0400

    day03

---
+++ src/test/resources/Queries/day03/having.sql
@@ -0,0 +1,33 @@
+select * from employees;
+
+
+-- Display job_ids where their avg salary is more than 5K
+select * from EMPLOYEES;
+
+-- CORRECT RESULT
+select JOB_ID,avg(SALARY),count(*) from EMPLOYEES
+group by JOB_ID
+having avg(SALARY)>5000;
+
+-- BAD PRACTICE and NOT CORRECT
+select JOB_ID,avg(SALARY),count(*) from EMPLOYEES
+                          where SALARY>5000
+group by JOB_ID;
+
+-- get me department_id where employees count bigger than 5
+select DEPARTMENT_ID,count(*) from EMPLOYEES
+group by DEPARTMENT_ID
+having count(*)<5
+;
+
+
+
+
+
+
+
+
+
+
+
+
