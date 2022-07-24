10bf4805f9452906012988562387a5455464a738
Mehmet <mhmtsytrk@gmail.com>
2022-07-24 10:57:17 -0400

    day05

---
+++ src/test/resources/Queries/day05/joinExercises.sql
@@ -0,0 +1,61 @@
+select * from employees;
+
+
+--1. Display all first_name and department_name
+    -- Steven -- Executive
+    -- Neena  -- Executive
+select  * from employees;
+select * from departments;
+-- join and inner join is same
+select FIRST_NAME,DEPARTMENT_NAME from EMPLOYEES e join  DEPARTMENTS d
+     on e.DEPARTMENT_ID = d.DEPARTMENT_ID;
+
+
+--2.Display all first_name and department_name including the  department without employee
+select FIRST_NAME,d.DEPARTMENT_ID,DEPARTMENT_NAME
+from EMPLOYEES e right  join  DEPARTMENTS d
+     on e.DEPARTMENT_ID = d.DEPARTMENT_ID;
+
+--left join
+select FIRST_NAME,d.DEPARTMENT_ID,DEPARTMENT_NAME
+from DEPARTMENTS d left  join  EMPLOYEES e
+      on e.DEPARTMENT_ID = d.DEPARTMENT_ID;
+
+--3.Display all first_name and department_name including the  employee without department
+select  * from employees;
+select * from departments;
+
+select FIRST_NAME,DEPARTMENT_NAME from DEPARTMENTS d right join EMPLOYEES e
+    on d.DEPARTMENT_ID = e.DEPARTMENT_ID;
+
+-- left
+select FIRST_NAME,DEPARTMENT_NAME from EMPLOYEES e left join DEPARTMENTS d
+                                                                on d.DEPARTMENT_ID = e.DEPARTMENT_ID;
+
+--4.Display all first_name and department_name
+--including the  department without employee  and  employees  without departments
+
+select FIRST_NAME,DEPARTMENT_NAME from DEPARTMENTS d full join EMPLOYEES e
+    on d.DEPARTMENT_ID = e.DEPARTMENT_ID;
+
+
+--5.Display All cities and related country names
+select city,COUNTRY_NAME from LOCATIONS l inner join COUNTRIES c
+        on l.COUNTRY_ID = c.COUNTRY_ID;
+
+
+--6.Display All cities and related country names  including with countries without city
+select * from COUNTRIES;
+select * from locations;
+
+select city,country_name from LOCATIONS l right join COUNTRIES c
+    on l.COUNTRY_ID = c.COUNTRY_ID;
+
+--left
+select city,country_name from COUNTRIES c left join LOCATIONS l
+                                                     on l.COUNTRY_ID = c.COUNTRY_ID;
+
+
+--7.Display all department name and  street adresss
+--8.Display first_name,last_name and department_name,city for all employees
+--9.Display first_name,last_name and department_name,city,country_name for all employees
