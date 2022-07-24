8649812400e3c29c0057a669cfed0d1e577cd08a
Mehmet <mhmtsytrk@gmail.com>
2022-07-24 12:01:44 -0400

    day05

--- src/test/resources/Queries/day04/Joins.sql
+++ src/test/resources/Queries/day04/Joins.sql
@@ -65,4 +65,30 @@ from CUSTOMER C full join   ADDRESS A
                              ON C.ADDRESS_ID = A.ADDRESS_ID;
 
 
-select * from
+/*
+ LEFT OUTER WITH WHERE
+ -- Get me only unique for left table
+
+ NOTE --> If there is a customer with addressID 6 , since there is no matching data
+ from address table it will assing NULL value for address table address_id
+
+ That is why we are using WHERE with address.addrees_id IS NULL to get unique for LEFT table
+ */
+
+SELECT customer_id, first_name, last_name,customer.ADDRESS_ID,ADDRESS.ADDRESS_ID,phone
+FROM customer LEFT OUTER JOIN address
+                              ON customer.address_id = address.address_id
+WHERE ADDRESS.ADDRESS_ID IS NULL;
+
+
+/*
+
+ FULL OUTER WITH WHERE
+ -- get non-matching part of the tables
+
+ */
+
+SELECT customer_id, first_name, last_name,customer.ADDRESS_ID,ADDRESS.ADDRESS_ID,phone
+FROM customer FULL OUTER JOIN address
+                              ON customer.address_id = address.address_id
+WHERE ADDRESS.ADDRESS_ID IS NULL OR CUSTOMER.ADDRESS_ID IS NULL;
