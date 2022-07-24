6d877a7f67592b6d7c15008058458d5ef9320dca
Mehmet <mhmtsytrk@gmail.com>
2022-07-24 14:45:34 -0400

    day05

--- src/test/resources/Queries/day05/setOperators.sql
+++ src/test/resources/Queries/day05/setOperators.sql
@@ -1,3 +1,5 @@
+select * from employees;
+
 create table Developers(
                            Id_Number Integer primary key,
                            Names varchar(30),
@@ -12,9 +14,68 @@ create table Testers(
 insert into developers values (1, 'Mike', 155000);
 insert into developers values (2, 'John', 142000);
 insert into developers values (3, 'Steven', 850000);
+insert into developers values (5, 'Steven', 860000);
+insert into developers values (6, 'John', 870000);
+
 insert into developers values (4, 'Maria', 120000);
 insert into testers values (1, 'Steven', 110000);
 insert into testers values(2, 'Adam', 105000);
 insert into testers values (3, 'Lex', 100000);
 
 commit work;
+
+select *  from developers;
+select * from TESTERS;
+
+
+/*
+ UNION
+ - To merge more than table vertically
+ - Removes DUPs
+ - Make an order asc
+
+ */
+-- it checks all rows to find same row.if we have it will remove one of them
+ select  * from DEVELOPERS
+ UNION
+ select * from TESTERS;
+
+
+select names from DEVELOPERS
+UNION
+select names from TESTERS;
+
+/*
+ UNION ALL
+ - DONT Remove DUPs
+ - DONT sort results
+
+ */
+
+select  * from DEVELOPERS
+UNION ALL
+select * from TESTERS;
+
+
+select names from DEVELOPERS
+UNION ALL
+select names from TESTERS;
+
+-- MINUS
+select  * from DEVELOPERS
+MINUS
+select * from TESTERS;
+
+select  names from DEVELOPERS
+MINUS
+select names from TESTERS;
+
+-- INTERSECT
+select  * from DEVELOPERS
+INTERSECT
+select * from TESTERS;
+
+select  names from DEVELOPERS
+INTERSECT
+select names from TESTERS;
+
