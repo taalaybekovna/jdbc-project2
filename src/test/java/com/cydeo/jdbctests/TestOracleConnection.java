f8f278a6588b7a3c5c96d1d1cb8036885194b490
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-07-26 19:22:49 -0400

    CHANGES

--- src/test/java/com/cydeo/jdbctests/TestOracleConnection.java
+++ src/test/java/com/cydeo/jdbctests/TestOracleConnection.java
@@ -5,12 +5,12 @@ import java.sql.*;
 public class TestOracleConnection {
 
     /*
-     HOW TO INIT VARIABLE
+    HOW TO INIT VARIABLE
 
-     OPT+ENTER --> Introduce Local Variable --> MAC
-     ALT+ENTER --> Introduce Local Variable --> WIN
+    OPT+ENTER --> Introduce Local Varibale --> MAC
+    ALT+ENTER --> Introduce Local Variable --> WIN
 
-    */
+   */
     public static void main(String[] args) throws SQLException {
 
         String dbUrl="jdbc:oracle:thin:@54.211.225.58:1521:XE";
@@ -23,10 +23,11 @@ public class TestOracleConnection {
         // Statement helps us to execute Query
         Statement statement = conn.createStatement();
 
-        // ResultSet stores data that we get from after query execution
+        // ResetSet stores data that we get from after query execution
         // rs is just a variable/object name
 
         ResultSet rs = statement.executeQuery("select * from regions");
+
 
         /*
 
@@ -38,7 +39,8 @@ public class TestOracleConnection {
          */
 
         //next() --> move to cursor in next line
-        rs.next();
+        //true if the new current row is valid; false if there are no more rows
+        //rs.next();
 
         /*
          getInt( index ) will return Int if we have Int value in that column
@@ -48,11 +50,44 @@ public class TestOracleConnection {
 
          */
         // 1
-        System.out.println(rs.getInt(1));
-        System.out.println(rs.getInt("REGION_ID"));
+        //System.out.println(rs.getInt(1));
+        //System.out.println(rs.getInt("REGION_ID"));
+
+        // get me region name
+        //System.out.println(rs.getString(2));
+        //System.out.println(rs.getString("REGION_NAME"));
 
 
+        //move to the 2 row
+        //rs.next();
+
+        // 2 - Americas
+        //System.out.println(rs.getInt("REGION_ID")+" - "+rs.getString("REGION_NAME"));
+
+        //move to the 3 row
+        //rs.next();
+
+        // 3 - Asia
+        //System.out.println(rs.getInt("REGION_ID")+" - "+rs.getString("REGION_NAME"));
+
+        //move to the 4 row
+        //rs.next();
+
+        // 4 - Middle East and Africa
+        //System.out.println(rs.getInt("REGION_ID")+" - "+rs.getString("REGION_NAME"));
+
+
+
+
+        while(rs.next()){
+            System.out.println(rs.getInt("REGION_ID")+" - "+rs.getString("REGION_NAME"));
+        }
+
+
+        //close conn
+        rs.close();
+        statement.close();
+        conn.close();
 
     }
 }
-
